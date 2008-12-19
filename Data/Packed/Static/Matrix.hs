8909bfd42ae11171fffb10397b765a92b1875230
Reiner Pope <reiner.pope@gmail.com>
2008-12-19 18:15:37 +1030

    More documentation for Matrix and Vector
    darcs-hash:20081219074537-7eff5-ff4203782dbab8036cc19fc02836e0d14f40c773.gz

--- Data/Packed/Static/Matrix.hs
+++ Data/Packed/Static/Matrix.hs
@@ -95,9 +95,29 @@ forgetRowsU = unsafeReshape
 forgetColsU :: Matrix (m,n) t -> Matrix (m,Unknown) t
 forgetColsU = unsafeReshape
 
+{- | Fixes a matrix's static row length.
+Essentially a mechanism for partial type signatures:
+you can specify the row length without specifying the 
+rest of matrix's type.
+
+@\>ident \`atRows\` d5
+[$mat| 1.0, 0.0, 0.0, 0.0, 0.0;
+       0.0, 1.0, 0.0, 0.0, 0.0;
+       0.0, 0.0, 1.0, 0.0, 0.0;
+       0.0, 0.0, 0.0, 1.0, 0.0;
+       0.0, 0.0, 0.0, 0.0, 1.0 |]@
+-}
 atRows :: Matrix (m,n) t -> m -> Matrix (m,n) t
 atRows = const
 
+{- | Fixes a matrix's static column length.
+
+@\> ident \`atCols\` d4
+[$mat| 1.0, 0.0, 0.0, 0.0;
+       0.0, 1.0, 0.0, 0.0;
+       0.0, 0.0, 1.0, 0.0;
+       0.0, 0.0, 0.0, 1.0 |]@
+-}
 atCols :: Matrix (m,n) t -> n -> Matrix (m,n) t
 atCols = const
 
@@ -119,101 +139,309 @@ withSquare = ($ n) where
     n = fromIntegerT (undefined :: n)
 
 --------- to/from lists
+{- | Constructs a matrix from a list. The size
+in the matrix's type and the list's length must agree or
+else a runtime error will be raised.
+
+@\> matFromList [1,2,3,4,5,6] \`atShape\` (d2,d3)
+[$mat| 1.0, 2.0, 3.0;
+       4.0, 5.0, 6.0 |]@
+-}
 matFromList :: (Element t, PositiveT m, PositiveT n) => [t] -> Matrix (m,n) t
 matFromList xs = withShape (\m n -> Matrix $ (m H.>< n) xs)
 
+{- | Constructs a matrix from a list. The size
+in the matrix's type and the list's length must agree or
+else a runtime error will be raised.
+
+@\> (d2 >< d3)[1,2,3,4,5,6]
+[$mat| 1.0, 2.0, 3.0;
+       4.0, 5.0, 6.0 |]@
+-}
 (><) :: (PositiveT m, PositiveT n, Element t) => m -> n -> [t] -> Matrix (m,n) t
 (><) m n xs = Matrix $ ((fromIntegerT m) H.>< (fromIntegerT n)) xs
 
+{- | Constructs a matrix from a list of lists of elements.
+Each sublist must be of equal size or a runtime error
+will be raised.
+
+@\> fromListsU [[1,2,3],[4,5,6]]
+[$mat| 1.0, 2.0, 3.0;
+       4.0, 5.0, 6.0 |]@
+-}
 fromListsU :: (Element t) => [[t]] -> Matrix (Unknown,Unknown) t
 fromListsU = wrapU . H.fromLists
 
+{- | Converts a matrix to a list of its rows, each as
+a list.
+
+@\> toLists [$mat|1,2,3;4,5,6|]
+[[1.0,2.0,3.0],[4.0,5.0,6.0]]@
+-}
 toLists :: (Element t) => Matrix (m,n) t -> [[t]]
 toLists = H.toLists . unMatrix
 
 --------- to/from row/column vectors
+{- | Interprets a vector as a 1-row matrix. 
+
+@\> asRow [$vec|1,2,3|]
+[$mat| 1.0, 2.0, 3.0 |]@
+-}
 asRow :: (Element a) => Vector n a -> Matrix (D1,n) a
 asRow = Matrix . H.asRow . unVector
 
+{- | Interprets a vector as a 1-column matrix.
+
+@\> asColumn [$vec|1,2,3|]
+[$mat| 1.0;
+       2.0;
+       3.0 |]@
+-}
 asColumn :: (Element a) => Vector n a -> Matrix (n,D1) a
 asColumn = Matrix . H.asColumn . unVector
 
+{- | Constructs a matrix from a list of rows.
+
+@\> fromRowsU [[$vec|1,2,3|],[$vec|4,5,6|]]
+[$mat| 1.0, 2.0, 3.0;
+       4.0, 5.0, 6.0 |]@
+-}
 fromRowsU :: (Element t) => [Vector n t] -> Matrix (Unknown,n) t
 fromRowsU = Matrix . H.fromRows . map unVector
 
+{- | Converts a matrix to a list of its rows.
+
+@\> toRows [$mat|1,2,3;4,5,6|]
+[[$vec| 1.0, 2.0, 3.0 |],[$vec| 4.0, 5.0, 6.0 |]]@
+-}
 toRows :: (Element t) => Matrix (m,n) t -> [Vector n t]
 toRows = map Vector . H.toRows . unMatrix
 
+{- | Constructs a matrix from a list of columns.
+
+@\> fromColumnsU [[$vec|1,2,3|],[$vec|4,5,6|]]
+[$mat| 1.0, 4.0;
+       2.0, 5.0;
+       3.0, 6.0 |]@
+-}
 fromColumnsU :: (Element t) => [Vector n t] -> Matrix (n,Unknown) t
 fromColumnsU = Matrix . H.fromColumns . map unVector
 
+{- | Converts a matrix to a list of its columns.
+
+@\> toColumns [$mat|1,2,3;4,5,6|]
+[[$vec| 1.0, 4.0 |],[$vec| 2.0, 5.0 |],[$vec| 3.0, 6.0 |]]@
+-}
 toColumns :: (Element t) => Matrix (m,n) t -> [Vector m t]
 toColumns = map Vector . H.toColumns . unMatrix
 
 -------- other operations
+{- | Returns the number of rows of the matrix.
+
+@\> rows [$mat|1::Double,2,3;4,5,6|]
+2@
+-}
 rows :: Matrix (m,n) t -> Int
 rows = H.rows . unMatrix
 
+{- | Returns the number of columns of the matrix.
+
+@\> cols [$mat|1::Double,2,3;4,5,6|]
+3@
+-}
 cols :: Matrix (m,n) t -> Int
 cols = H.cols . unMatrix
 
+{- | Matrix transpose.
+
+@\> trans [$mat|1,2,3;4,5,6|]
+[$mat| 1.0, 4.0;
+       2.0, 5.0;
+       3.0, 6.0 |]@
+-}
 trans :: Matrix (m,n) t -> Matrix (n,m) t
 trans = Matrix . H.trans . unMatrix
 
 --- we can't disappear the Int argument... (it would require type division)
+{- | Reshapes a vector into a matrix, 
+with the specified number of columns. If the vector's length
+is not a multiple of the required columns,
+a runtime error is raised.
+
+@\> reshapeU 3 [$vecU|1,2,3,4,5,6|]
+[$mat| 1.0, 2.0, 3.0;
+       4.0, 5.0, 6.0 |]@
+-}
 reshapeU :: (Element t) => Int -> Vector n t -> Matrix (Unknown,Unknown) t
 reshapeU n = wrapU . H.reshape n . unVector
 
+{- | Flattens a matrix into a vector.
+
+@\> flatten [$mat|1,2,3;4,5,6|]
+[$vec| 1.0, 2.0, 3.0, 4.0, 5.0, 6.0 |]@
+-}
 flatten :: (Element t) => Matrix (m,n) t -> Vector (m :*: n) t
 flatten = Vector . H.flatten . unMatrix
 
+{- | Indexes a matrix.
+
+@\> [$mat|1,2,3;4,5,6|] \@\@\> (1,2)
+6.0@
+-}
 (@@>) :: (Storable t) => Matrix (m,n) t -> (Int, Int) -> t
 (@@>) = (H.@@>) . unMatrix
 
 --- I say the size is (Unknown,Unknown) so the users don't think they all
 --- have to be of the same size.
+{- | Constructs a matrix from blocks.
+
+@\> fromBlocksU [[[$matU|1,2,3;4,5,6|],  [$matU|7,8,9;10,11,12|]],
+               [[$matU|11,12,13;14,15,16|],[$matU|21,22,23;24,25,26|]]]
+[$mat|  1.0,  2.0,  3.0,  7.0,  8.0,  9.0;
+        4.0,  5.0,  6.0, 10.0, 11.0, 12.0;
+       11.0, 12.0, 13.0, 21.0, 22.0, 23.0;
+       14.0, 15.0, 16.0, 24.0, 25.0, 26.0 |]@
+-}
 fromBlocksU :: (Element t) => [[Matrix (Unknown,Unknown) t]] -> Matrix (Unknown,Unknown) t
 fromBlocksU = Matrix . H.fromBlocks . map (map unMatrix)
 
+{- | 'replicate' for matrices.
+
+@\> repmatU [$mat|1;2|] 2 3
+[$mat| 1.0, 1.0, 1.0;
+       2.0, 2.0, 2.0;
+       1.0, 1.0, 1.0;
+       2.0, 2.0, 2.0 |]@
+-}
 repmatU :: (Element t) => Matrix (m,n) t -> Int -> Int -> Matrix (Unknown,Unknown) t
 repmatU m i j = Matrix $ H.repmat (unMatrix m) i j
 
+{- | Vertically flips a matrix.
+
+@\> flipud [$mat|1,2,3;4,5,6|]
+[$mat| 4.0, 5.0, 6.0;
+       1.0, 2.0, 3.0 |]@
+-}
 flipud :: (Element t) => Matrix (m,n) t -> Matrix (m,n) t
 flipud = Matrix . H.flipud . unMatrix
 
+{- | Horizonatlly flips a matrix.
+
+@\> fliprl [$mat|1,2,3;4,5,6|]
+[$mat| 3.0, 2.0, 1.0;
+       6.0, 5.0, 4.0 |]@
+-}
 fliprl :: (Element t) => Matrix (m,n) t -> Matrix (m,n) t
 fliprl = Matrix . H.fliprl . unMatrix
 
+{- | Extracts a submatrix.
+
+@\> subMatrixU (0,1) (2,2) [$mat|1,2,3,4;5,6,7,8;9,10,11,12|]
+[$mat| 2.0, 3.0;
+       6.0, 7.0 |]@
+-}
 subMatrixU :: (Element a) => (Int, Int) -> (Int, Int) -> Matrix (m,n) a -> Matrix (Unknown,Unknown) a
 subMatrixU a b = Matrix . H.subMatrix a b . unMatrix
 
+{- | Takes rows from the top of the matrix 
+until the required size is reached.
+
+@\> takeRows [$mat|1,2;3,4;5,6|] \`atRows\` d2
+[$mat| 1.0, 2.0;
+       3.0, 4.0 |]@
+-}
 takeRows :: (PositiveT m', Element t, (m' :<=: m) ~ True) => Matrix (m,n) t -> Matrix (m',n) t
 takeRows a = withRows (\m -> Matrix $ H.takeRows m $ unMatrix a)
 
+{- | Takes rows from the bottom of the matrix until
+the required size is reached.
+
+@\> dropRows [$mat|1,2;3,4;5,6|] \`atRows\` d2
+[$mat| 3.0, 4.0;
+       5.0, 6.0 |]@
+-}
 dropRows :: (PositiveT m', Element t, (m' :<=: m) ~ True) => Matrix (m,n) t -> Matrix (m',n) t
 dropRows a = withRows (\m -> Matrix $ H.dropRows (rows a - m) $ unMatrix a)
 
+{- | Takes columns from the left of the matrix until
+the required size is reached.
+
+@\> takeColumns [$mat|1,2,3;4,5,6|] \`atCols\` d2
+[$mat| 1.0, 2.0;
+       4.0, 5.0 |]@
+-}
 takeColumns :: (PositiveT n', Element t, (n' :<=: n) ~ True) => Matrix (m,n) t -> Matrix (m,n') t
 takeColumns a = withCols (\n -> Matrix $ H.takeColumns n $ unMatrix a)
 
+{- | Takes columns from the right of the matrix
+until the required size is reached.
+
+@\> dropColumns [$mat|1,2,3;4,5,6|] \`atCols\` d2
+[$mat| 2.0, 3.0;
+       5.0, 6.0 |]@
+-}
 dropColumns :: (PositiveT n', Element t, (n' :<=: n) ~ True) => Matrix (m,n) t -> Matrix (m,n') t
 dropColumns a = withCols (\n -> Matrix $ H.dropColumns (cols a - n) $ unMatrix a)
 
+{- | Extracts the given rows from a matrix.
+
+@\> extractRowsU [1,0] [$mat|1,2;3,4;5,6|]
+[$mat| 3.0, 4.0;
+       1.0, 2.0 |]@
+-}
 extractRowsU :: (Element t) => [Int] -> Matrix (m,n) t -> Matrix (Unknown,n) t
 extractRowsU is = Matrix . H.extractRows is . unMatrix
 
+{- | Constructs the identity matrix of any given size.
+
+@\> ident \`atRows\` d3
+[$mat| 1.0, 0.0, 0.0;
+       0.0, 1.0, 0.0;
+       0.0, 0.0, 1.0 |]@
+-}
 ident :: (Element t, PositiveT n) => Matrix (n,n) t
 ident = withSquare (Matrix . H.ident)
 
+{- | Constructs a square matrix with the given vector as its diagonal.
+
+@\> diag (linspace (1,3)) \`atRows\` d3
+[$mat| 1.0, 0.0, 0.0;
+       0.0, 2.0, 0.0;
+       0.0, 0.0, 3.0 |]@
+-}
 diag :: (Element a) => Vector n a -> Matrix (n,n) a
 diag = Matrix . H.diag . unVector
 
+{- | Constructs a rectangular matrix with the given vector as its diagonal.
+
+@\> diagRect (linspace (1,3)) \`atShape\` (d3,d4)
+[$mat| 1.0, 0.0, 0.0, 0.0;
+       0.0, 2.0, 0.0, 0.0;
+       0.0, 0.0, 3.0, 0.0 |]
+
+\> diagRect (linspace (1,3)) \`atShape\` (d4,d3)
+[$mat| 1.0, 0.0, 0.0;
+       0.0, 2.0, 0.0;
+       0.0, 0.0, 3.0;
+       0.0, 0.0, 0.0 |]@
+-}
 diagRect :: (Element a, PositiveT m, PositiveT n) => Vector (Min m n) a -> Matrix (m,n) a
 diagRect v = withShape (\m n -> Matrix $ H.diagRect (unVector v) m n)
 
+{- | Takes the diagonal from a matrix.
+
+@\> takeDiag [$mat|1,2,3;4,5,6|]
+[$vec| 1.0, 5.0 |]@
+-}
 takeDiag :: (Element t) => Matrix (m,n) t -> Vector (Min m n) t
 takeDiag = Vector . H.takeDiag . unMatrix
 
+{- | Operations on matrices viewed as operations on the vector of their elements 
+
+@\> liftMatrix (+constant 2) [$mat|1,2,3;4,5,6|]
+[$mat| 3.0, 4.0, 5.0;
+       6.0, 7.0, 8.0 |]@
+-}
 liftMatrix :: (Element a, Element b) => (Vector (m :*: n) a -> Vector (m :*: n) b) -> Matrix (m,n) a -> Matrix (m,n) b
 liftMatrix f = Matrix . H.liftMatrix (unVector . f . Vector) . unMatrix
 
--- Data/Packed/Static/Vector.hs
+++ Data/Packed/Static/Vector.hs
@@ -17,8 +17,8 @@ module Data.Packed.Static.Vector(
    -- * Shaping
    -- | Functions manipulating a vector's (static) shape.
    refineVec,
-   withDim,
    atDim,
+   atShape,
    -- * To/from lists
    fromListU,
    toList,
@@ -69,58 +69,137 @@ withDim :: forall n a. PositiveT n => (Int -> Vector n a) -> Vector n a
 withDim f = f n where
     n = fromIntegerT (undefined :: n)
 
+{- | Sets an arbitrary-length vector to a specific value.
+
+@\> constant 1 `atDim` 5
+[$vec| 1.0, 1.0, 1.0, 1.0, 1.0 |]@
+-}
 atDim :: (forall n. PositiveT n => Vector n t) -> Int -> Vector Unknown t
 atDim v n | n > 0     = fromJust $ reifyPositiveD (toInteger n) (\n -> forgetShapeU $ v `atShape` n)
           | otherwise = error $ "atDim: negative vector length: " ++ show n
 
 -------- To / from lists
--- | Constructs a vector from all the elements of a list.
+{- | Constructs a vector from all the elements of a list.
+
+@\> fromListU [1,2,3,4,5]
+[$vec| 1.0, 2.0, 3.0, 4.0, 5.0 |]@
+-}
 fromListU :: (Storable a) => [a] -> Vector Unknown a
 fromListU = Vector . H.fromList
 
--- | Converts to a list of elements.
+{- | Converts to a list of elements.
+
+@\> toList [$vec|1,2,3|]
+[1.0,2.0,3.0]@
+-}
 toList :: (Storable a) => Vector n a -> [a]
 toList = H.toList . unVector
 
 ------ Other operations
--- | Vector's length.
+{- | Vector's length.
+
+@\> dim [$vec|1::Double,2,3|]
+3@
+-}
 dim :: Vector n t -> Int
 dim = H.dim . unVector
 
+{- | Indexes a vector.
+
+@\> [$vec|1,2,3|] \@\> 1
+2.0@
+-}
 (@>) :: (Storable t) => Vector n t -> Int -> t
 (@>) = (H.@>) . unVector
 
-subVectorU :: (Storable t) => Int -> Int -> Vector n t -> Vector Unknown t
+{- | Extracts a subvector.
+
+@\> subVectorU 2 3 [$vec|1,2,3,4,5|]
+[$vec| 3.0, 4.0, 5.0 |]@
+-}
+subVectorU :: (Storable t) => 
+              Int -- ^ Initial index
+           -> Int -- ^ Length of resultant vector
+           -> Vector n t
+           -> Vector Unknown t
 subVectorU a b = Vector . H.subVector a b . unVector
 
 --- I say the input Size is unknown, although it can be anything,
 --- so users don't think all the vectors must be of the same size.
+
+{- | Joins each vector in the list.
+
+@\> joinU [[$vecU|1,2,3|], [$vecU|4,5|]]
+[$vec| 1.0, 2.0, 3.0, 4.0, 5.0 |]@
+-}
 joinU :: (Storable t) => [Vector Unknown t] -> Vector Unknown t
 joinU = Vector . H.join . map unVector
 
+{- | Creates a constant vector of any length. The length is
+determined by the type.
+
+@\> [$vec|1,2,3|] + constant 2
+[$vec| 3.0, 4.0, 5.0 |]@
+-}
 constant :: (Element t, PositiveT n) => t -> Vector n t
 constant a = withDim (Vector . H.constant a)
 
+{- | Creates a vector of arbitrary length whose
+components range linearly from a to b. The vector's
+length is determined by its type.
+
+@\> linspace (1,5) `atShape` d4
+[$vec| 1.0, 2.333333333333333, 3.6666666666666665, 5.0 |]@
+-}
 linspace :: (PositiveT n) => (Double,Double) -> Vector n Double
 linspace r = withDim (\n -> Vector $ H.linspace n r) where
 
+{- | Gives the vector's minimum entry.
+
+@\> vectorMin [$vec|1,2,3|]
+1.0@
+-}
 vectorMin :: Vector n Double -> Double
 vectorMin = H.vectorMin . unVector
 
+{- | Gives the vector's maximum entry.
+
+@\> vectorMax [$vec|1,2,3|]
+3.0@
+-}
 vectorMax :: Vector n Double -> Double
 vectorMax = H.vectorMax . unVector
 
+{- | Gives the index of a vector's minimum entry.
+@\> vectorMinIndex [$vec|1,2,3|]
+0@
+-}
 vectorMinIndex :: Vector n Double -> Int
 vectorMinIndex = H.vectorMinIndex . unVector
 
+{- | Gives the index of a vector's maximum entry. 
+
+@\> vectorMaxIndex [$vec|1,2,3|]
+2@
+-}
 vectorMaxIndex :: Vector n Double -> Int
 vectorMaxIndex = H.vectorMaxIndex . unVector
 
+{- | 'map' for vectors.
+
+@\> (*2) `liftVector` [$vec|1,2,3|]
+[$vec| 2.0, 4.0, 6.0 |]@
+-}
 liftVector :: (Storable a, Storable b) => (a -> b) -> Vector n a -> Vector n b
 liftVector f = Vector . H.liftVector f . unVector
 
 --- note: this requires they are of the same size, whereas hmatrix allows
 --- different sizes; it uses the minimum size.
+{- | 'zipWith' for vectors. 
+
+@\> liftVector2 (+) [$vec|1,2,3|] (constant 3)
+[$vec| 4.0, 5.0, 6.0 |]@
+-}
 liftVector2 :: (Storable a, Storable b, Storable c) =>
                (a -> b -> c) -> Vector n a -> Vector n b -> Vector n c
 liftVector2 f v1 v2 = Vector $ H.liftVector2 f (unVector v1) (unVector v2)
